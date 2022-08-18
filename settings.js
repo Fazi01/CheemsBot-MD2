950f6a478277309435e0bf1d2244fd3689b9d615
Fazi01 <111182049+Fazi01@users.noreply.github.com>
2022-08-18 09:04:00 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
-global.premium = ['916909137213']
-global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
-global.botname = 'Cheems Bot MD V2'
+global.owner = ['6288271609722']
+global.premium = ['6288271609722']
+global.ownernomer = '6288271609722'
+global.ownername = 'Fazi'
+global.botname = 'Fzii'
 global.footer = 'Xeon Bot Inc.'
 global.ig = 'https://github.com/DGXeon'
 global.region = 'India, NorthEast, Mizoram'
 global.sc = 'https://github.com/DGXeon/CheemsBot-MD2'
 global.myweb = 'https://youtu.be/imFIX-Wrt3s'
-global.packname = 'Cheems Bot'
-global.author = '🦄Dream Guy Xeon'
+global.packname = 'FAZI'
+global.author = '🦄FZI'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
